9c96baad74e157790016e52e09f98dc83f3bfff3
Mathieu Roy <mathieuroy1983@gmail.com>
2023-11-13 13:13:56 -0500

    main argc ount wrong

--- main.cpp
+++ main.cpp
@@ -3,5 +3,5 @@
 int main(int argc, char **argv)
 {
     if (argc != 3)
-        throw std::runtime_error("Error: invalid number of arguments\n");
+        throw std::runtime_error("Error: invalid number of arguments should be: ./ircserv <port> <password>\n");
 }
