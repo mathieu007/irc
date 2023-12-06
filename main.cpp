92af23c34ecdd5f84d8ae0736559e4d767043399
Mathieu Roy <mathieuroy1983@gmail.com>
2023-12-06 16:15:33 -0500

    first commit

--- main.cpp
+++
@@ -1,7 +0,0 @@
-#include <Server.hpp>
-
-int main(int argc, char **argv)
-{
-    if (argc != 3)
-        throw std::runtime_error("Error: invalid number of arguments should be: ./ircserv <port> <password>\n");
-}
