f595e13f0e6176eabaabc1eef283552b42a25775
Christopher Perron <cperron@c2r6p4.42quebec.com>
2023-12-12 16:39:09 -0500

    yo

---
+++ objs/src/Execution/Channel.d
@@ -0,0 +1,24 @@
+objs/src/Execution/Channel.o: src/Execution/Channel.cpp \
+  include/Execution/Channel.hpp include/Execution/Client.hpp \
+  include/Execution/Map.hpp include/Execution/Vec.hpp \
+  include/Execution/ClientChannelMapping.hpp \
+  include/Execution/Message.hpp include/Execution/Server.hpp \
+  include/Execution/String.hpp include/Execution/Logger.hpp
+
+include/Execution/Channel.hpp:
+
+include/Execution/Client.hpp:
+
+include/Execution/Map.hpp:
+
+include/Execution/Vec.hpp:
+
+include/Execution/ClientChannelMapping.hpp:
+
+include/Execution/Message.hpp:
+
+include/Execution/Server.hpp:
+
+include/Execution/String.hpp:
+
+include/Execution/Logger.hpp:
